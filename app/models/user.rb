d44a2214613d249a0696d93ddbd97b992730afec
kosuke <ikkorokoro@icloud.com>
2020-06-06 09:57:07 +0000

    delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: true#アドレス大小関係なくなる
+                      uniqueness: true
    has_secure_password
    validates :password, presence: true, length: { minimum: 6 }
 end
