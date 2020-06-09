b1ef625e709d76b40a1cff3106f6a3af80a6bfd8
kosuke <ikkorokoro@icloud.com>
2020-06-09 04:41:01 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+   host = 'https://f4dfe024c067439b9ce82fca053d465b.vfs.cloud9.ap-northeast-1.amazonaws.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # localhostで開発している場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
