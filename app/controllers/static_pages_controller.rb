738dd2b1355895b6daa5bd1c3f6df40623327223
kosuke <ikkorokoro@icloud.com>
2020-06-11 01:41:06 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+   if logged_in?#current_userがnilの場合にメソッドmicropostsを渡してもエラーになるためif logged_in?を確認する    @micropost = current_user.microposts.build
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
+   end
   end
 
   def help
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show 
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -51,16 +52,7 @@ class UsersController < ApplicationController
    def user_params
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
-   # beforeアクション
 
-    # ログイン済みユーザーかどうか確認
-   def logged_in_user
-    unless logged_in?
-    store_location
-    flash[:dander] = "Plese log in"
-    redirect_to login_url
-    end
-   end
    # 正しいユーザーかどうか確認
    def correct_user
     @user = User.find(params[:id])
